bf13f6c81168b8177117921e896bb7210fc428ff
Valter Rogério da Silva <40283319+valterr28@users.noreply.github.com>
2020-10-13 17:51:26 -0300

    A

---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+
+import './database/connection'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/users', (request, response) => {
+    
+    return response.json( {message: 'hello'} );
+})
+
+
+
+
+app.listen(3333)
+
